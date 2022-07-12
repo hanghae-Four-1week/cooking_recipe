c3f76fac13a33e508c73236264fa842ff5da49c0
minimi61 <new1573@naver.com>
2022-07-12 13:58:04 +0900

    db store ok

--- app.py
+++ app.py
@@ -4,25 +4,28 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 
-client = MongoClient('mongodb+srv://sparta:woowa@Cluster0.lqx8f.mongodb.net/?retryWrites=true&w=majority', 27017, username="sparta", password="woowa")
+client = MongoClient('mongodb+srv://sparta:woowa@cluster0.lqx8f.mongodb.net/?retryWrites=true&w=majority', 27017, username="sparta", password="woowa")
 db = client.dbcooking_recipe
 
 
-@app.route('/')
+# 모든 데이터 뽑아보기
+
+@app.route('/main')
 def main():
-    return render_template("index.html")
+    dishes_list = list(db.dishes.find({}, {'_id': False}))
+    return jsonify({'dishes':dishes_list})
 
-@app.route("/detail", methods=["POST"])
-def post_dishes():
-    sample_receive = request.form['sample_give']
-    print(sample_receive)
-    return jsonify({'msg':'POST 연결 완료!'})
-
-@app.route('/detail', methods=["GET"])
-def get_dishes():
-    # 요리 목록을 반환하는 API
-    # return jsonify({'result': 'success', 'recipe_list': []})
-    return jsonify({'msg': 'GET 연결 완료!'})
-
+# @app.route("/detail", methods=["POST"])
+# def post_dishes():
+#     sample_receive = request.form['sample_give']
+#     print(sample_receive)
+#     return jsonify({'msg':'POST 연결 완료!'})
+#
+# @app.route('/detail', methods=["GET"])
+# def get_dishes():
+#     # 요리 목록을 반환하는 API
+#     # return jsonify({'result': 'success', 'recipe_list': []})
+#     return jsonify({'msg': 'GET 연결 완료!'})
+#
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
--- scraping.py
+++ scraping.py
@@ -3,15 +3,9 @@ from bs4 import BeautifulSoup
 from pymongo import MongoClient
 
 
-client = MongoClient('mongodb+srv://sparta:woowa@Cluster0.lqx8f.mongodb.net/?retryWrites=true&w=majority', 27017, username="sparta", password="woowa")
+
+client = MongoClient('mongodb+srv://sparta:woowa@cluster0.lqx8f.mongodb.net/?retryWrites=true&w=majority', 27017, username="sparta", password="woowa")
 db = client.dbcooking_recipe
-
-doc = {
-    'name':'bob',
-    'age':27
-}
-
-db.users.insert_one(doc)
 
 # URL을 읽어서 HTML를 받아오고,
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
@@ -23,12 +17,20 @@ soup = BeautifulSoup(data.text, 'html.parser')
 # select를 이용해서, tr들을 불러오기
 dishes = soup.select('#contents_area_full > ul > ul >li ')
 
+
 for dish in dishes:
     # dishes 안에 dish 가 있으면,
     dish_img = dish.select_one('div.common_sp_thumb > a > img')['src']
     dish_title = dish.select_one('div.common_sp_caption > div.common_sp_caption_tit.line2').text
-    dish_star = dish.select_one('div.common_sp_caption > div.common_sp_caption_rv > span.common_sp_caption_rv_star')
+    dish_star_tag = dish.select_one('div.common_sp_caption > div.common_sp_caption_rv > span.common_sp_caption_rv_star')
 
-    # if dish_title is not None:
-    #
-    #     print (dish_star)
+    # print(dish_star) 별점 크롤링 문제
+    doc = {
+        'dish_img': dish_img,
+        'dish_title': dish_title,
+        # 'dish_star': dish_star
+    }
+    db.dishes.insert_one(doc)
+
+
+
