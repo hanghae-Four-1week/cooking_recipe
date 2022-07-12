0ac10c629b9a09974d8fc1167a64aebc2b52fb7a
LuisKlopp <fbgus333@gmail.com>
2022-07-12 15:57:17 +0900

    main test

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from pymongo import MongoClient
 import jwt
 import datetime
 import hashlib
+import requests
+from bs4 import BeautifulSoup
 from flask import Flask, render_template, jsonify, request, redirect, url_for
 from werkzeug.utils import secure_filename
 from datetime import datetime, timedelta
@@ -13,17 +15,32 @@ app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
 
 SECRET_KEY = 'SPARTA'
 
-client = MongoClient('mongodb+srv://test:sparta@cluster0.xaxuh.mongodb.net/?retryWrites=true&w=majority')
+client = MongoClient(
+    'mongodb+srv://test:sparta@cluster0.xaxuh.mongodb.net/?retryWrites=true&w=majority')
 db = client.dbsparta_plus_week4
 
 
 @app.route('/')
-def home():
+def main_recipe():
     token_receive = request.cookies.get('mytoken')
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        userid = (payload["id"])
+        user_info = db.users.find_one({"username": userid})
 
-        return render_template('index.html')
+        headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+        data = requests.get(
+            'https://www.10000recipe.com/recipe/list.html', headers=headers)
+
+        # HTML을 BeautifulSoup이라는 라이브러리를 활용해 검색하기 용이한 상태로 만듦
+        soup = BeautifulSoup(data.text, 'html.parser')
+
+        # select를 이용해서, tr들을 불러오기
+        dishes = soup.select('#contents_area_full > ul > ul > li ')
+        print(dishes)
+
+        return render_template('index.html', user = user_info)
     except jwt.ExpiredSignatureError:
         return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
     except jwt.exceptions.DecodeError:
@@ -36,28 +53,30 @@ def login():
     return render_template('login.html', msg=msg)
 
 
-@app.route('/user/<username>')
-def user(username):
-    # 각 사용자의 프로필과 글을 모아볼 수 있는 공간
-    token_receive = request.cookies.get('mytoken')
-    try:
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        status = (username == payload["id"])  # 내 프로필이면 True, 다른 사람 프로필 페이지면 False
+# @app.route('/user/<username>')
+# def user(username):
+#     # 각 사용자의 프로필과 글을 모아볼 수 있는 공간
+#     token_receive = request.cookies.get('mytoken')
+#     try:
+#         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+#         # 내 프로필이면 True, 다른 사람 프로필 페이지면 False
+#         status = (username == payload["id"])
 
-        user_info = db.users.find_one({"username": username}, {"_id": False})
-        return render_template('user.html', user_info=user_info, status=status)
-    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-        return redirect(url_for("home"))
+#         user_info = db.users.find_one({"username": username}, {"_id": False})
+#         return render_template('user.html', user_info=user_info, status=status)
+#     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+#         return redirect(url_for("home"))
 
 
+# --------------------로그인----------------------
 @app.route('/sign_in', methods=['POST'])
 def sign_in():
-    # 로그인
     username_receive = request.form['username_give']
     password_receive = request.form['password_give']
 
     pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
-    result = db.users.find_one({'username': username_receive, 'password': pw_hash})
+    result = db.users.find_one(
+        {'username': username_receive, 'password': pw_hash})
 
     if result is not None:
         payload = {
@@ -65,32 +84,29 @@ def sign_in():
             'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
         }
         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
-
         return jsonify({'result': 'success', 'token': token})
-    # 찾지 못하면
     else:
         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
 
+
+# --------------------회원가입-----------------------
 @app.route('/sign_up/save', methods=['POST'])
 def sign_up():
     username_receive = request.form['username_give']
     password_receive = request.form['password_give']
     nickname_receive = request.form['nickname_give']
-    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    password_hash = hashlib.sha256(
+        password_receive.encode('utf-8')).hexdigest()
     doc = {
         "username": username_receive,                               # 아이디
-        "nick-name": nickname_receive,
+        "nickname": nickname_receive,                              # 닉네임
         "password": password_hash,                                  # 비밀번호
-        "profile_name": username_receive,                           # 프로필 이름 기본값은 아이디
-        "profile_pic": "",                                          # 프로필 사진 파일 이름
-        "profile_pic_real": "profile_pics/profile_placeholder.png", # 프로필 사진 기본 이미지
-        "profile_info": ""                                          # 프로필 한 마디
     }
     db.users.insert_one(doc)
     return jsonify({'result': 'success'})
 
-
+# --------------------아이디 중복확인-----------------------
 @app.route('/sign_up/check_dup', methods=['POST'])
 def check_dup():
     username_receive = request.form['username_give']
@@ -141,6 +157,23 @@ def update_like():
         return jsonify({"result": "success", 'msg': 'updated'})
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
         return redirect(url_for("home"))
+
+
+
+
+
+@app.route("/detail", methods=["POST"])
+def post_dishes():
+    sample_receive = request.form['sample_give']
+    print(sample_receive)
+    return jsonify({'msg': 'POST 연결 완료!'})
+
+
+@app.route('/detail', methods=["GET"])
+def get_dishes():
+    # 요리 목록을 반환하는 API
+    # return jsonify({'result': 'success', 'recipe_list': []})
+    return jsonify({'msg': 'GET 연결 완료!'})
 
 
 if __name__ == '__main__':
---
+++ scraping.py
@@ -0,0 +1,46 @@
+import requests
+from bs4 import BeautifulSoup
+from pymongo import MongoClient
+
+
+client = MongoClient(
+    'mongodb+srv://test:sparta@cluster0.xaxuh.mongodb.net/?retryWrites=true&w=majority')
+db = client.dbcooking_recipe
+
+doc = {
+    'name': 'bob',
+    'age': 35
+}
+
+db.recipe.insert_one(doc)
+
+# URL을 읽어서 HTML를 받아오고,
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get(
+    'https://www.10000recipe.com/recipe/list.html', headers=headers)
+
+# HTML을 BeautifulSoup이라는 라이브러리를 활용해 검색하기 용이한 상태로 만듦
+soup = BeautifulSoup(data.text, 'html.parser')
+
+# select를 이용해서, tr들을 불러오기
+dishes = soup.select('#contents_area_full > ul > ul > li ')
+
+for dish in dishes:
+    # dishes 안에 dish 가 있으면,
+    dish_img = dish.select_one('div.common_sp_thumb > a > img')['src']
+    dish_title = dish.select_one(
+        'div.common_sp_caption > div.common_sp_caption_tit.line2').text
+    dish_star = dish.select_one(
+        'div.common_sp_caption > div.common_sp_caption_rv > span.common_sp_caption_rv_star')
+
+    doc = {
+      'img' : dish_img,
+      'title' : dish_title,
+      'star' : dish_star
+    }
+
+    db.dishes.insert_one(doc)
+    # if dish_title is not None:
+    #
+    #     print (dish_star)
