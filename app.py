b18fbf4f0c7e8fe3c46509e3152e57b5005581bb
LuisKlopp <fbgus333@gmail.com>
2022-07-13 16:38:50 +0900

    좀만더 힘내자

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ from bs4 import BeautifulSoup
 from flask import Flask, render_template, jsonify, request, redirect, url_for
 from werkzeug.utils import secure_filename
 from datetime import datetime, timedelta
+from markupsafe import escape
+from bson.json_util import loads, dumps
 
 
 app = Flask(__name__)
@@ -29,7 +31,7 @@ def main_recipe():
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
         userid = (payload["id"])
         user_info = db.users.find_one({"username": userid})
-        recipes_list = list(db.recipes.find({}, {'_id': False}))
+        recipes_list = list(db.recipes.find({}))
         return render_template('index.html', user = user_info, recipes_list = recipes_list)
     except jwt.ExpiredSignatureError:
         return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
@@ -40,7 +42,7 @@ def main_recipe():
 def get_recipes():
     # 맛집 목록을 반환하는 API
     recipes_list = list(db.recipes.find({}, {'_id': False}))
-    print(recipes_list)
+    # print(recipes_list)
     # recipes_list 라는 키 값에 레시피 목록을 담아 클라이언트에게 반환합니다.
     return jsonify({'result': 'success', 'recipes_list': recipes_list})
 
@@ -114,63 +116,77 @@ def check_dup():
     return jsonify({'result': 'success', 'exists': exists})
 
 
-# @app.route('/update_profile', methods=['POST'])
-# def save_img():
-#     token_receive = request.cookies.get('mytoken')
-#     try:
-#         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-#         # 프로필 업데이트
-#         return jsonify({"result": "success", 'msg': '프로필을 업데이트했습니다.'})
-#     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-#         return redirect(url_for("home"))
+@app.route('/main/type', methods=['GET'])
+# db에서 키워드로 db리스트 가져오기
+def show_card():
+    keyword = request.args.get('type')
+    menu_card = list(db.recipes.find({'type': {'$regex': keyword}})) # $regex -> 포함된 문자열을 리스트 가져오기
+    return jsonify({'menu_card': dumps(menu_card)}) #dump() -> JSON문자열로 변환, id값이 $oid안에 있어서?
+
+@app.route('/posting', methods=['POST'])
+def posting():
+    # 클라이언트로부터 'comment_give'받아서 'comment_receive'에 넣어주기
+    comment_receive = request.form["comment_give"]
+    print(comment_receive, "댓글")
+    # 클라이언트로부터 'date_give'받아서 'date_receive'에 넣어주기
+    date_receive = request.form["date_give"]
+    # 'date_receive'를 문자열로 변환하기
+    date_receive = repr(date_receive)
+    print(date_receive, "날짜")
+    # 클라이언트로부터 'title_give' 받아서 'title_give'에 넣기
+    title_give = request.form['title_give']
+    print(title_give, "타이틀")
+    # 쿠키에서 토큰받기
+    token_receive = request.cookies.get('mytoken')
+    try:
+        # 받은 'token'에서 'id,exp'이 있는 payload 받아오기
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        # payload['id']에서 userid 받아오기
+        userid = (payload["id"])
+        # db의 user에서 userid 받아와서 user_info에 넣기
+        user_info = db.users.find_one({"userid": userid})
+        print(user_info,"유저인포")
+        # user_name, comment, date, title 들을 posts라는 db에 저장
+        doc = {
+            'user_name':user_info['username'],
+            "comment": comment_receive,
+            "date": date_receive,
+            'title': title_give
+        }
+        db.posts.insert_one(doc)
+        return jsonify({"result": "success", 'msg': '포스팅 성공'})
+    # jwt에서 exp가 만료되었을 때, "show_menu"로 보내기
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("show_menu"))
 
 
-# @app.route('/posting', methods=['POST'])
-# def posting():
-#     token_receive = request.cookies.get('mytoken')
-#     try:
-#         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-#         # 포스팅하기
-#         return jsonify({"result": "success", 'msg': '포스팅 성공'})
-#     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-#         return redirect(url_for("home"))
 
 
-# @app.route("/get_posts", methods=['GET'])
-# def get_posts():
-#     token_receive = request.cookies.get('mytoken')
-#     try:
-#         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-#         # 포스팅 목록 받아오기
-#         return jsonify({"result": "success", "msg": "포스팅을 가져왔습니다."})
-#     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-#         return redirect(url_for("home"))
+@app.route("/detail/<i>")
+def post_dishes(i):
 
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        userid = (payload["id"])
+        user_info = db.users.find_one({"username": userid})
+        recipes_list = list(db.recipes.find({'id': i}))
+        print(recipes_list)
+        return render_template('detail.html', user = user_info, recipes_list = recipes_list)
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+# @app.route("/theme/<theme_name>", methods=["POST"])
+# def travel_read(theme_name):
+#     travel_list = list(db.travels.find({'theme': theme_name}, {'_id': False}))
+#     return jsonify({'travels': travel_list})
 
-# @app.route('/update_like', methods=['POST'])
-# def update_like():
-#     token_receive = request.cookies.get('mytoken')
-#     try:
-#         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-#         # 좋아요 수 변경
-#         return jsonify({"result": "success", 'msg': 'updated'})
-#     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-#         return redirect(url_for("home"))
-
-
-
-
-@app.route("/detail", methods=["POST"])
-def post_dishes():
-    sample_receive = request.form['sample_give']
-    return jsonify({'msg': 'POST 연결 완료!'})
-
-
-@app.route('/detail', methods=["GET"])
-def get_dishes():
-    # 요리 목록을 반환하는 API
-    # return jsonify({'result': 'success', 'recipe_list': []})
-    return jsonify({'msg': 'GET 연결 완료!'})
+# @app.route('/detail', methods=["GET"])
+# def get_dishes():
+#     # 요리 목록을 반환하는 API
+#     # return jsonify({'result': 'success', 'recipe_list': []})
+#     return jsonify({'msg': 'GET 연결 완료!'})
 
 
 if __name__ == '__main__':
